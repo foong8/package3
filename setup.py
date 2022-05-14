5122f95c9c9895d31403f5a202903ac3122ad9df
lai hon foong <honfoong@hotmail.my>
2022-05-14 20:28:18 +0800

    version 2

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
-
 import setuptools
 
-setuptools.setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='honfoong',
+    version='0.0.3',
+    author='Hon Foong',
+    author_email='honfoong@hotmail.my',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/foong8/package3.git',
+    project_urls = {
+        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+    },
+    license='MIT',
+    packages=['honfoong'],
+)
